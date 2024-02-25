2a73764191a7dad44f44f8e7fcc6eab5e8280a97
Bagus Dwi Putra <bagusdp2011@gmail.com>
2024-02-25 15:07:24 +0700

    Feat: Before collaborator

---
+++ app/Http/Controllers/ControllerListItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerListItem extends Controller
+{
+    //
+}
---
+++ resources/views/List_Barang.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <div>
+        <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+    <h1>List Barang</h1>
+    <p>Kode Barang: {{$id}}</p>
+    <p>Nama Barang: {{$nama}}</p>
+    </div>
+
+</html>
