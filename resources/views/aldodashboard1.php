78663ba317957347644b200bef0e514255eba679
leonaldo21 <aldoea12@gmail.com>
2024-05-09 13:56:34 +0700

    aldodashboard1

--- resources/views/aldodashboard1.php
+++ resources/views/aldodashboard1.php
@@ -1,94 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Admin Dashboard</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-    html,body,h1,h2,h3,h4,h5 {
-        font-family: "Raleway", sans-serif;
-        margin: 0;
-        padding: 0;
-    }
-
-    .sidebar {
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 300px;
-        background-color: #fff;
-        padding-top: 20px;
-        z-index: 1000;
-        overflow-x: hidden;
-        transition: 0.5s;
-    }
-
-    .content {
-        margin-left: 300px;
-        padding: 20px;
-    }
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Dashboard</title>
+    <!-- Link to Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Link to Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body>
+<body class="bg-gray-100">
 
 <!-- Top container -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+<nav class="bg-gray-900 text-white">
+    <div class="container mx-auto py-4">
+        <button class="block lg:hidden focus:outline-none">
+            <i class="fa fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Logo</a>
-                </li>
-            </ul>
+        <div class="hidden lg:flex items-center justify-between">
+            <a class="text-lg font-semibold" href="#">Logo</a>
         </div>
     </div>
 </nav>
 
 <!-- Sidebar/menu -->
-<div class="sidebar">
-    <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <img src="/w3images/avatar2.png" class="img-fluid rounded-circle" alt="Profile Picture" style="width:46px">
-            </div>
-            <div class="col-8">
-                <span>Welcome, <strong>name</strong></span><br>
-                <a href="#" class="btn btn-link"><i class="fa fa-envelope"></i></a>
-                <a href="#" class="btn btn-link"><i class="fa fa-user"></i></a>
-                <a href="#" class="btn btn-link"><i class="fa fa-cog"></i></a>
+<div class="flex flex-col bg-white w-64 h-screen shadow-lg fixed top-0 left-0 transition-all duration-500 z-50">
+    <div class="p-4 flex items-center">
+        <img src="/w3images/avatar2.png" class="rounded-full w-12 h-12" alt="Profile Picture">
+        <div class="ml-4">
+            <span class="block font-semibold">Welcome, <strong>name</strong></span>
+            <div class="flex mt-1">
+                <a href="#" class="mr-2 text-gray-600 hover:text-gray-900"><i class="fa fa-envelope"></i></a>
+                <a href="#" class="mr-2 text-gray-600 hover:text-gray-900"><i class="fa fa-user"></i></a>
+                <a href="#" class="text-gray-600 hover:text-gray-900"><i class="fa fa-cog"></i></a>
             </div>
         </div>
-        <hr>
-        <div class="container">
-            <h5>Dashboard</h5>
-        </div>
-        <div class="container">
-            <a href="#" class="btn btn-primary btn-block"><i class="fa fa-users fa-fw"></i> Menu</a>
-            <a href="#" class="btn btn-light btn-block"><i class="fa fa-address-book fa-fw"></i> Contact</a>
-            <a href="#" class="btn btn-light btn-block"><i class="fa fa fa-bank fa-fw"></i> Wallet</a>
-            <a href="#" class="btn btn-light btn-block"><i class="fa fa-history fa-fw"></i> History</a>
-            <a href="#" class="btn btn-light btn-block"><i class="fa fa-cog fa-fw"></i> Settings</a>
-        </div>
+    </div>
+    <hr class="my-2">
+    <div class="p-4">
+        <h5 class="font-semibold">Dashboard</h5>
+        <a href="#" class="block py-2 px-4 mt-2 bg-blue-500 text-white rounded-lg text-center hover:bg-blue-600">Menu</a>
+        <a href="#" class="block py-2 px-4 mt-2 bg-gray-200 text-gray-800 rounded-lg text-center hover:bg-gray-300">Contact</a>
+        <a href="#" class="block py-2 px-4 mt-2 bg-gray-200 text-gray-800 rounded-lg text-center hover:bg-gray-300">Wallet</a>
+        <a href="#" class="block py-2 px-4 mt-2 bg-gray-200 text-gray-800 rounded-lg text-center hover:bg-gray-300">History</a>
+        <a href="#" class="block py-2 px-4 mt-2 bg-gray-200 text-gray-800 rounded-lg text-center hover:bg-gray-300">Settings</a>
     </div>
 </div>
 
 <!-- Page content -->
-<div class="content">
+<div class="ml-64 p-8">
     <header>
-        <h5><i class="fa fa-dashboard"></i> My Dashboard</h5>
+        <h5 class="text-lg font-semibold"><i class="fa fa-dashboard"></i> My Dashboard</h5>
     </header>
     <!-- Your content here -->
 </div>
 
-<!-- Bootstrap JS and dependencies -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Tailwind CSS and dependencies -->
+<!-- You may need to add additional JavaScript dependencies based on your Tailwind CSS configuration -->
+<!-- For example, to enable dark mode: <script src="https://cdn.tailwindcss.com/versions/2.2.19/tailwind-dark.min.js"></script> -->
 </body>
 </html>
