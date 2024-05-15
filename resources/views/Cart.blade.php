34f8f3b6bd30d3d3538904fd5c0596cb168924ee
Sheeryl <serilvioleta@gmail.com>
2024-05-15 08:30:47 +0700

    cart

--- resources/views/Cart.blade.php
+++ resources/views/Cart.blade.php
@@ -4,28 +4,29 @@
 <head>
     <title>Mr Beans Login</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('css/keranjang.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <!-- Remove Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}" /> -->
 </head>
 
 <body>
     @include('header') <!-- Include header.blade.php -->
 
     <!-- Isi  -->
-    <div class="wrapper text-center" style="margin-top:100px;">
-        <p style="font-size: 30px;"><b>KERANJANG SAYA</b></p>
+    <div class="wrapper text-center mt-24">
+        <p class="text-2xl font-bold">KERANJANG SAYA</p>
     </div>
-    <div class="container-fluid table-container">
-        <div class="col-md-8">
-            <table class="table table-striped">
+    <div class="container mx-auto table-container">
+        <div class="w-full md:w-2/3 mx-auto">
+            <table class="table-auto w-full">
                 <thead>
-                    <tr class="bg-secondary text-white text-center" style="font-weight: bold;">
-                        <td>No</td>
-                        <td>Nama Barang</td>
-                        <td>Harga </td>
-                        <td>Quantity </td>
-                        <td>Aksi</td>
+                    <tr class="bg-gray-600 text-white text-center font-bold">
+                        <th class="p-2">No</th>
+                        <th class="p-2">Nama Barang</th>
+                        <th class="p-2">Harga</th>
+                        <th class="p-2">Quantity</th>
+                        <th class="p-2">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,14 +42,14 @@
                     @endphp
 
                     @foreach ($query as $data)
-                    <tr class="text-center">
-                        <td>{{ $no++ }}</td>
-                        <td>{{ $data->namaProduk }}</td>
-                        <td>Rp. {{ number_format($data->harga) }}</td>
-                        <td>{{ $data->quantity }} pcs</td>
-                        <td>
-                            <a href="{{ route('add.cart', ['id' => $data->produkID]) }}" class="btn btn-primary tambah-btn float-end">Tambah</a>
-                            <a href="{{ route('delete.cart', ['id' => $data->produkID]) }}" class="btn btn-danger float-end btn-delete" onclick="return confirm('Yakin???')">Hapus</a>
+                    <tr class="text-center border-t">
+                        <td class="p-2">{{ $no++ }}</td>
+                        <td class="p-2">{{ $data->namaProduk }}</td>
+                        <td class="p-2">Rp. {{ number_format($data->harga) }}</td>
+                        <td class="p-2">{{ $data->quantity }} pcs</td>
+                        <td class="p-2 flex justify-center space-x-2">
+                            <a href="{{ route('add.cart', ['id' => $data->produkID]) }}" class="btn bg-blue-500 text-white px-4 py-2 rounded">Tambah</a>
+                            <a href="{{ route('delete.cart', ['id' => $data->produkID]) }}" class="btn bg-red-500 text-white px-4 py-2 rounded" onclick="return confirm('Yakin???')">Hapus</a>
                         </td>
                         @php
                         $totalharga += ($data->harga * $data->quantity);
@@ -58,25 +59,20 @@
                 </tbody>
             </table>
             <br>
-            <div class="row">
-                <div class="col-md-8">
-                </div>
-                <div class="col-md-4 text-right">
-                    <p class="text-end">Total harga = Rp. {{ number_format($totalharga) }}</p>
-                    <form action="{{ route('konfirmasi') }}" method="POST">
-                        @csrf <!-- CSRF protection -->
-                        <input type="hidden" name="userID" value="{{ session('userID') }}">
-                        @if(isset($quantity))
-                            <input type="hidden" name="quantity" value="{{ $quantity }}">
-                        @else
-                            <input type="hidden" name="quantity" value="0"> 
-                        @endif
-                        <input type="hidden" name="totalharga" value="{{ $totalharga }}">
-                        <button type="submit" class="btn btn-primary float-end">Bayar</button>
-                    </form>
-
-                </div>
+            <div class="flex justify-end mt-4">
+                <p class="text-right">Total harga = Rp. {{ number_format($totalharga) }}</p>
             </div>
+            <form action="{{ route('konfirmasi') }}" method="POST" class="flex justify-end mt-4">
+                @csrf <!-- CSRF protection -->
+                <input type="hidden" name="userID" value="{{ session('userID') }}">
+                @if(isset($quantity))
+                    <input type="hidden" name="quantity" value="{{ $quantity }}">
+                @else
+                    <input type="hidden" name="quantity" value="0"> 
+                @endif
+                <input type="hidden" name="totalharga" value="{{ $totalharga }}">
+                <button type="submit" class="btn bg-blue-500 text-white px-4 py-2 rounded">Bayar</button>
+            </form>
         </div>
     </div>
     @include('footer')
